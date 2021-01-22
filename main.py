3e560852ff85b7beb1fda409acd07eaa3e16227b
Colton Gotham <cgotham24@biomedscienceacademy.org>
2021-01-22 20:16:25 +0000

    I wrote 2 more questions but one of them are still broken

--- main.py
+++ main.py
@@ -97,9 +97,10 @@ def main():
   
   feeling = str(input("\n\nHow are you today?\n")).lower()
 
-  happy_emotions = ["happy","good", "great", "well", "amazing", "beautiful","happy!","good!", "great!", "well!", "amazing!", "beautiful!"]
+  happy_emotions = ["happy","good", "great", "well", "amazing", "beautiful","wonderful","happy!","good!", "great!", "well!", "amazing!", "beautiful!", "wonderful!"]
 
-  sad_emotions = ["bad", "sad", "not great", "not good", "no good", "depressed"]
+  sad_emotions = ["bad", "sad", "not great", "not good", "no good", "depressed", "unhappy", "lonely", "gloomy"]
+
 
   mad_emotions = ["mad", "angry", "upset", "annoyed"]
 
@@ -123,8 +124,87 @@ def main():
     print (f"I hope you will be able to fill your {feeling}ness")
 
   else:
-    print ("Okay")
+    print ("Sorry I don't understand...")
   
+  #2011-2025
+  generation_alpha = ["2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"]
+
+  #1998-2010
+  generation_z = ["1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"]
+
+  #1981-1997
+  millennial = ["1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997"]
+
+  #1965-1980
+  generation_x = ["1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980"]
+
+  #1946-1964
+  baby_boomer = ["1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964"]
+
+  #1928-1945
+  generation_silent = ["1928","1929","1930","1931","1932","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945"]
+
+  #1901-1927
+  generation_greatest = ["1901","1902","1903","1904","1905","1906","1907","1908","1909","1910","1911","1912","1913","1914","1915","1916","1917","1918","1919","1920","1921","1922","1923","1924","1925","1926","1927"]
+  
+  age = input("\nWhat year were you born?\n")
+
+  if age in generation_alpha:
+    print (f"Okay so if you were born in {age} that means you are generation Alpha")
+
+  elif age in generation_z:
+    print (f"Okay so if you were born in {age} that means you are Generation Z")
+
+  elif age in millennial:
+    print (f"Okay so if you were born in {age} that means you are a Millennial")
+
+  elif age in generation_x:
+    print (f"Okay so if you were born in {age} that means you are Generation X")
+  
+  elif age in baby_boomer:
+    print (f"Okay so if you were born in {age} that means you are a Baby Boomer")
+  
+  elif age in generation_silent:
+    print (f"Okay so if you were born in {age} that means you are the Silent Generation") 
+  
+  elif age in generation_greatest:
+    print (f"Okay so if you were born in {age} that means you are the Greatest Generation")
+
+  else:
+    print (f"Okay so if you were born in {age} you are either a zombie or a time traveler?!")
+  
+  color = input("\nWhat is your favorite color?\n").lower
+
+  if color == ("red"):
+    print (f"Okay so if your color is {color} then you probably like apples or cherries")
+  elif color == ("blue"):
+    print (f"Okay so if your color is {color} then you probably like birds or the ocean ")
+  elif color == ("green"):
+    print (f"Okay so if your color is {color} then you probably like plants or turtles")
+  elif color == ("yellow"):
+    print (f"Okay so if your color is {color} then you probably like pichachu or bananas")
+  elif color == ("pink"):
+    print (f"Okay so if your color is {color} then you probably like flamingos or cherry blossoms")
+  elif color == ("purple"):
+    print (f"Okay so if your color is {color} then you probably like grapes or butterflys")
+  elif color == ("orange"):
+    print (f"Okay so if your color is {color} then you probably like sunsets or basketball")
+  elif color == ("brown"):
+    print (f"Okay so if your color is {color} then you probably like bears or chocolate")
+  elif color == ("white"):
+    print (f"Okay so if your color is {color} then you probably like snow or seashells")
+  elif color == ("black"):
+    print (f"Okay so if your color is {color} then you probably like penguins or cats")
+  else:
+    print (f"If your color is {color} then I am not familer with that color\n")
+
+
+
+
+
+
+  
+
   
 
 
